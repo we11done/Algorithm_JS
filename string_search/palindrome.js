84eda62300c03d29a5cbac0a5e81327ed87fb77f
Jaehyun Jun <wjswogus0007@gmail.com>
2022-01-12 15:54:14 -0500

    solved

---
+++ string_search/palindrome.js
@@ -0,0 +1,24 @@
+// Write a function that returns true if given string is a palindrome,
+// false other wise. the function should not be case sensitive.
+// Ex) INPUT: gooG , OUTPUT: true
+// Ex2) INPUT: good, OUTPUT: false
+// Ex3) INPUT: found7 time study Yduts emit 7Dnuof, OUTPUT: true
+const solution = string => {
+  // Solved with for loop
+  // let half = Math.floor(string.length / 2);
+  // let firstHalf = '';
+  // let secondHalf = '';
+  // for (let i = 0; i < half; i++) {
+  //   firstHalf += string.charAt(i);
+  //   secondHalf += string.charAt(string.length - 1 - i);
+  // }
+  // return firstHalf === secondHalf;
+  // Solved with split & join
+  return (
+    string.toUpperCase() === string.toUpperCase().split('').reverse().join('')
+  );
+};
+
+console.log(solution('goog'));
+console.log(solution('good'));
+console.log(solution('found7 time study Yduts emit 7Dnuof'));
