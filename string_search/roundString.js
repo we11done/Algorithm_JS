01b3401f21b7ca2c140e21a5722a7cb82953d035
Jaehyun Jun <wjswogus0007@gmail.com>
2022-01-12 15:42:59 -0500

    solved:

--- string_search/roundString.js
+++ string_search/roundString.js
@@ -1,10 +1,10 @@
-// Write a function that returns true if given string is a round string
+// Write a function that returns true if given string is a round string,
 // false other wise.
 // Ex) INPUT: goog , OUTPUT: true
 // Ex2) INPUT: good, OUTPUT: false
 
 const solution = string => {
-  let half = Math.ceil(string.length / 2);
+  let half = Math.floor(string.length / 2);
   let firstHalf = '';
   let secondHalf = '';
   for (let i = 0; i < half; i++) {
