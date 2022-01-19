ec7ed35a61c4a8de8f8427b2d86d7e9810d53f66
Jaehyun Jun <wjswogus0007@gmail.com>
2022-01-19 15:25:09 -0500

    solved with stack and added performance measuring

--- stack_queue/parenthesis.js
+++ stack_queue/parenthesis.js
@@ -5,21 +5,16 @@
 // Ex) INPUT: '(()(()))(()'
 //     OUTPUT: false
 
-// Using built-in string functions
 const solution = string => {
-  if (string.charAt(0) === ')') return false;
-  return (
-    string.split('').filter(s => s === '(').length ===
-    string.split('').filter(s => s === ')').length
-  );
-};
-
-// Using stacks
-const solution2 = string => {
   if (string.charAt(0) === ')') return false;
   let stack = [];
   for (let x of string) {
-    x === '(' ? stack.push(x) : stack.pop();
+    if (x === '(') stack.push(x);
+    else {
+      if (stack.pop() === undefined) {
+        return false;
+      }
+    }
   }
   return stack.length === 0;
 };
@@ -28,7 +23,3 @@ let t = performance.now();
 let a1 = solution('(()(()))(()');
 console.log('Solution 1 took ', performance.now() - t, ' seconds');
 console.log('Solution 1 answer:', a1);
-let t2 = performance.now();
-let a2 = solution2('(()(()))(()');
-console.log('Solution 2 took ', performance.now() - t2, ' seconds');
-console.log('Solution 2 answer:', a2);
