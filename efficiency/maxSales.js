a67a561051a97e152874ed30faaee4bf1a5a97b3
Jaehyun Jun <wjswogus0007@gmail.com>
2022-01-18 19:34:07 -0500

    feat: performance measuring added

--- efficiency/maxSales.js
+++ efficiency/maxSales.js
@@ -5,6 +5,7 @@
 // Ex) INPUT: records = [12 ,15 ,11 ,20 ,25 ,10 ,20 ,19 ,13 ,15], k = 3
 //     OUPUT: 56 ( 11 + 20 + 25)
 
+// Nested for loop: O(N^2)
 const solution = (records, k) => {
   let maxSales = Number.MIN_SAFE_INTEGER;
   for (let i = 0; i < records.length - 2; i++) {
@@ -17,4 +18,25 @@ const solution = (records, k) => {
   return maxSales;
 };
 
-console.log(solution([12, 15, 11, 20, 25, 10, 20, 19, 13, 15], 3));
+// Sliding window algorithm: O(N);
+const solution2 = (records, k) => {
+  let maxSales = (window = records
+    .slice(0, k)
+    .reduce((acc, current) => acc + current, 0));
+  let leftIndex = 0;
+  for (let i = k; i < records.length; i++) {
+    let currentSales = window + records[i] - records[leftIndex++];
+    if (maxSales < currentSales) maxSales = currentSales;
+    window = currentSales;
+  }
+  return maxSales;
+};
+
+let t = performance.now();
+let a1 = solution([12, 15, 11, 20, 25, 10, 20, 19, 13, 15], 3);
+console.log('Solution 1 took ', performance.now() - t, ' seconds');
+console.log('Solution 1 answer:', a1);
+let t2 = performance.now();
+let a2 = solution2([12, 15, 11, 20, 25, 10, 20, 19, 13, 15], 3);
+console.log('Solution 2 took ', performance.now() - t2, ' seconds');
+console.log('Solution 2 answer:', a2);
