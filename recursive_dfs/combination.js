d1b4e9e44416e3d98f92755b9007202cdc9eb62d
Jaehyun Jun <wjswogus0007@gmail.com>
2022-04-01 23:35:15 -0400

    added another solution with memoization

--- recursive_dfs/combination.js
+++ recursive_dfs/combination.js
@@ -5,16 +5,28 @@
 //     INPUT: n = 33, r = 19
 //     OUTPUT: 818809200
 
+// without memoization
 const solution = (n, r) => {
   if (r === 0 || n === r) return 1;
   return solution(n - 1, r - 1) + solution(n - 1, r);
 };
 
+// with memoization
+const solutionWithMemoization = (n, r) => {
+  const memo = Array.from(Array(r + 1), () => Array(n + 1).fill(0));
+  const combinations = (n, r) => {
+    if (memo[r][n] > 0) return memo[r][n];
+    if (r === 0 || n === r) return 1;
+    return (memo[r][n] = combinations(n - 1, r - 1) + combinations(n - 1, r));
+  };
+  return combinations(n, r);
+};
+
 let t = performance.now();
-let a1 = solution(5, 3);
+let a1 = solution(33, 19);
 console.log('Solution took ', performance.now() - t, ' seconds');
 console.log('Solution answer:', a1);
 let t2 = performance.now();
-let a2 = solution(33, 19);
-console.log('Solution took ', performance.now() - t2, ' seconds');
-console.log('Solution answer:', a2);
+let a2 = solutionWithMemoization(33, 19);
+console.log('Solution with memo took ', performance.now() - t2, ' seconds');
+console.log('Solution with memo answer:', a2);
